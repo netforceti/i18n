e7be12459505c8f9bd7e172ee9af98c40a42183e
Bruno Gonçalves <bugotech@gmail.com>
2021-07-07 22:38:03 -0300

    add home saudades

---
+++ langs/pt_br/home.js
@@ -0,0 +1,7 @@
+module.exports = {
+    saudacoes: {
+        bomdia  : 'Bom dia',
+        boatarde: 'Boa tarde',
+        boanoite: 'Boa noite',
+    }
+};
--- langs/pt_br/index.js
+++ langs/pt_br/index.js
@@ -1,2 +1,3 @@
 module.exports = {
+    home : require('./home'),
 };
